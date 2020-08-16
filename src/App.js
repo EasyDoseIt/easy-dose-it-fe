c9e49babf992d43530d7cf3447e389f118ad6846
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-08-15 19:27:20 -0500

    updated logos and added splash screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import SplashScreen from '../src/components//SplashScreen/SplashScreen.js';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 5000);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {isLoading === false ? (
+        <div className="App">
+
+        </div>
+      ) : (
+        <div className="App">
+          <SplashScreen />
+        </div>
+          )}
+    </>
   );
 }
 
