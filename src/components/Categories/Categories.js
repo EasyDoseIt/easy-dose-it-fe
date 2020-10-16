90834f691d37837fa6bebe45da3a8aeb3907c2d1
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-10-15 22:04:58 -0500

    categories view

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Categories.css';
+const Categories = () => {
+    return (
+        <main className="categories-container">
+            <div className='category'><h3 className="category-title">Sleepy Time</h3></div>
+            <div className='category'><h3 className="category-title">Period Pain</h3></div>
+            <div className='category'><h3 className="category-title">Post Chemo Nausea</h3></div>
+            <div className='category'><h3 className="category-title">Hunger Busters</h3></div>
+        </main>
+    )
+}
+
+export default Categories;
